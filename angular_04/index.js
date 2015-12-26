f055f6e94f946ab8c7726969f2766559df5b5f34
yichengjie <626659321@qq.com>
2015-12-26 22:55:16 +0800

    先commit

--- angular_04/index.js
+++ angular_04/index.js
@@ -1,9 +1,24 @@
 /**
  * Created by Administrator on 2015/11/14.
  */
-$(function () {
-    //$('#startTime').datepicker(); //绑定输入框
-}) ;
+$(function(){
+    $('.select > p').on('click', function(e){
+        $('.select').toggleClass('open');
+        e.stopPropagation();
+    });
+    $('.select ul li').on('click', function(e){
+        var _this = $(this);
+        $('.select > p').text(_this.attr('data-value'));
+        _this.addClass('selected').siblings().removeClass('selected');
+        $('.select').removeClass('open');
+        e.stopPropagation();
+    });
+    $(document).on('click', function(){
+        $('.select').removeClass('open');
+    });
+});
+
+
 
 var app = angular.module('app',[]) ;
 app.controller('IndexController', function ($scope) {
